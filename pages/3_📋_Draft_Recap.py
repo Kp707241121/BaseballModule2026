5ea78de79ea4751a4cf213e994fa73f93c54bab2
Kp707241121 <kp707241121@gmail.com>
2026-06-03 19:42:19 -0400

    update
    chagnedEvlis

--- pages/3_📋_Draft_Recap.py
+++ pages/3_📋_Draft_Recap.py
@@ -9,16 +9,18 @@ summary = get_draft_summary()
 
 # Define static team order (manual or preserved)
 # Define your desired team order
-teams = ["Hoern Dogs", 
+teams = [
+    "Hoern Dogs", 
          "Steven's Smart Team", 
          "So Much (For) Fooly Cooly", 
-         "The Fighting Elvises",
+         "The Fighting Elvises ",
          "Ramiritek", 
          "Jameis' Crab Shack", 
          "Mike's Magnificent Team", 
          "Dodger Dogs", 
          "Wahoo Lives", 
-         "No Schlitt, Sherlock!"]  # Replace with actual team names in desired order
+         "No Schlitt, Sherlock!"
+         ]  # Replace with actual team names in desired order
 
 # Get all unique round numbers
 rounds = sorted({rnd for team in summary.values() for rnd in team})
